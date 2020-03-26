95791642b254a9a2393b00778647c916ff6ec5d9
panyongjie <286422681@qq.com>
2020-03-26 09:54:06 +0800

    前端

---
+++ js/index.js
@@ -0,0 +1,20 @@
+var baseUrl = "http://127.0.0.1:9010/"; //基础路径
+
+var MaterielPOS = "saas/Materiel/POS"; // 获取POS物料信息
+
+var MaterielJDE = "saas/Materiel/JDE"; //获取JDE物料信息
+
+var MaterielBefore = "saas/materiel/saveMateriel"; //映射
+
+var MaterielPOSAfter = "saas/Materiel/POSAfter"; // 获取POS物料信息
+
+var MaterielJDEAfter = "saas/Materiel/JDEAfter"; //获取JDE物料信息
+
+var PaymentJDE = "saas/Payment/JDE"; //获取JDE支付方式信息
+
+var PaymentPOS = "saas/Payment/POS"; //获取POS支付方式信息
+
+var savePayment = "saas/Payment/savePayment"; //映射
+
+var saleAmount = "saas/salesamount/sendAmount"; //手动推送销售和收款数据
+
